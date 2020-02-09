892e0b6138680820ea012a169406c658b26a1c39
kalte <renata@cyren.org>
2020-02-09 14:09:12 -0500

    asdf

--- messaging.js
+++ messaging.js
@@ -1,15 +1,63 @@
-const currentUser = Phoebe;
+
+let phoebe = {
+    password: "Polar_seltzer",
+    donor: true,
+    tampons: true,
+    pads: false,
+    latitude: "32",
+    longitude: "32"
+};
+
+let rebecca = {
+    password: "Polar_seltzer",
+    donor: "true",
+    heavyTampons: "1000",
+    regularTampons: "0",
+    latitude: "32",
+    longitude: "32"
+};
+
+let chelsea = {
+    password: "Polar_seltzer",
+    donor: "true",
+    heavyTampons: "1000",
+    regularTampons: "0",
+    latitude: "32",
+    longitude: "32"
+};
+
+let eliza = {
+    password: "Polar_seltzer",
+    donor: "true",
+    heavyTampons: "1000",
+    regularTampons: "0",
+    latitude: "32",
+    longitude: "32"
+};
+let angelica = {
+    password: "Polar_seltzer",
+    donor: "true",
+    heavyTampons: "1000",
+    regularTampons: "0",
+    latitude: "32",
+    longitude: "32"
+};
+
+var users={phoebe,rebecca,chelsea, eliza, angelica}
+const currentUser = phoebe;
 function getHeavyTampon() {
     /*const heavytampon = document.querySelector("#input-heavy-tampons").value;
     console.log(heavytampon);*/
     localStorage.setItem(Phoebe.heavyTampon, document.querySelector("#input-heavy-tampons").value);
 }
 function storeTampon() {
-    localStorage.setItem('need', 'tampon')
+    localStorage.setItem('need', 'tampon');
+    console.log(localStorage.getItem('need'));
 }
 
 function storePad() {
-    localStorage.setItem('need','pad')
+    localStorage.setItem('need','pad');
+    console.log(localStorage.getItem('need'));
 }
 
 
@@ -64,48 +112,23 @@ function match(productType)
       };
 
 }
-var users={phoebe,rebecca,chelsea, eliza, angelica}
-let phoebe = {
-    password: "Polar_seltzer",
-    donor: true,
-    tampons=true,
-    pads=false,
-    latitude: "32",
-    longitude: "32"
-};
 
-let rebecca = {
-    password: "Polar_seltzer",
-    donor: "true",
-    heavyTampons: "1000",
-    regularTampons: "0",
-    latitude: "32",
-    longitude: "32"
-};
 
-let chelsea = {
-    password: "Polar_seltzer",
-    donor: "true",
-    heavyTampons: "1000",
-    regularTampons: "0",
-    latitude: "32",
-    longitude: "32"
-};
 
-let eliza = {
-    password: "Polar_seltzer",
-    donor: "true",
-    heavyTampons: "1000",
-    regularTampons: "0",
-    latitude: "32",
-    longitude: "32"
-};
-let angelica = {
-    password: "Polar_seltzer",
-    donor: "true",
-    heavyTampons: "1000",
-    regularTampons: "0",
-    latitude: "32",
-    longitude: "32"
-};
+var x = document.getElementById("demo");
+            
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+
+
+
 }
