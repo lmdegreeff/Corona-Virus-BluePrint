b192b3b3e2fcd1247f0d7d60512619f585e04ea4
kalte <renata@cyren.org>
2020-02-09 13:37:42 -0500

    ASD

--- messaging.js
+++ messaging.js
@@ -1,60 +1,16 @@
+const currentUser = Phoebe; 
 function getHeavyTampon() {
     /*const heavytampon = document.querySelector("#input-heavy-tampons").value;
     console.log(heavytampon);*/
     localStorage.setItem(Phoebe.heavyTampon, document.querySelector("#input-heavy-tampons").value);
 }
-function match(productType)
-{
-    for 
+function storeTampon() {
+    localStorage.setItem('need', 'tampon')
 }
-users={phoebe,rebecca,chelsea, eliza, angelica}
-let phoebe = {
-    password: "Polar_seltzer",
-    donor: true,
-    heavyTampons: true,
-    regularTampons: false,
-    lightTampons: true,
-    heavyPad: true,
-    regularPad: false,
-    lightPad: false,
-    latitude: "32",
-    longitude: "32"
-};
 
-let rebecca = {
-    password: "Polar_seltzer",
-    donor: "true",
-    heavyTampons: "1000",
-    regularTampons: "0",
-    latitude: "32",
-    longitude: "32"
-};
-
-let chelsea = {
-    password: "Polar_seltzer",
-    donor: "true",
-    heavyTampons: "1000",
-    regularTampons: "0",
-    latitude: "32",
-    longitude: "32"
-};
-
-let eliza = {
-    password: "Polar_seltzer",
-    donor: "true",
-    heavyTampons: "1000",
-    regularTampons: "0",
-    latitude: "32",
-    longitude: "32"
-};
-let angelica = {
-    password: "Polar_seltzer",
-    donor: "true",
-    heavyTampons: "1000",
-    regularTampons: "0",
-    latitude: "32",
-    longitude: "32"
-};
+function storePad() {
+    localStorage.setItem('need','pad')
+}
 
 
 
