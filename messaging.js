e4c5297995bba35b20669565ad1137dec10d27af
kalte <renata@cyren.org>
2020-02-09 12:06:53 -0500

    adsf

--- messaging.js
+++ messaging.js
@@ -4,8 +4,25 @@ function getHeavyTampon() {
     console.log(heavytampon);*/
     localStorage.setItem(Phoebe.heavyTampon, document.querySelector("#input-heavy-tampons").value);
 }
+function hasProduct(productType)
+{
+    
+}
 
 let phoebe = {
+    password: "Polar_seltzer",
+    donor: true,
+    heavyTampons: true,
+    regularTampons: false,
+    lightTampons: true,
+    heavyPad: true,
+    regularPad: false,
+    lightPad: false,
+    latitude: "32",
+    longitude: "32"
+};
+
+let rebecca = {
     password: "Polar_seltzer",
     donor: "true",
     heavyTampons: "1000",
@@ -13,3 +30,32 @@ let phoebe = {
     latitude: "32",
     longitude: "32"
 };
+
+let chelsea = {
+    password: "Polar_seltzer",
+    donor: "true",
+    heavyTampons: "1000",
+    regularTampons: "0",
+    latitude: "32",
+    longitude: "32"
+};
+
+let eliza = {
+    password: "Polar_seltzer",
+    donor: "true",
+    heavyTampons: "1000",
+    regularTampons: "0",
+    latitude: "32",
+    longitude: "32"
+};
+let angelica = {
+    password: "Polar_seltzer",
+    donor: "true",
+    heavyTampons: "1000",
+    regularTampons: "0",
+    latitude: "32",
+    longitude: "32"
+};
+
+
+
