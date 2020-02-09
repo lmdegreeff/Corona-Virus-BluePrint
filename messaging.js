75f714747d4c3d1f3cfabaa04a2c59dfe01e1434
kalte <renata@cyren.org>
2020-02-09 10:52:07 -0500

    Update messaging.js

--- messaging.js
+++ messaging.js
@@ -1,5 +1,13 @@
+const currentUser = Phoebe; 
 function getHeavyTampon() {
     /*const heavytampon = document.querySelector("#input-heavy-tampons").value;
     console.log(heavytampon);*/
-    localStorage.setItem('heavytampon', document.querySelector("#input-heavy-tampons").value);
+    localStorage.setItem(Phoebe.heavyTampon, document.querySelector("#input-heavy-tampons").value);
+    Phoebe.heavyTampon
 }
+
+var Phoebe = {
+    password: "Polar_seltzer",
+    heavyTampon: "1000",
+    regularTampon: "0"
+};
