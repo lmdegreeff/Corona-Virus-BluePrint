017cf80568efbe468473fdf04853bf6fe21e0bba
kalte <renata@cyren.org>
2020-02-09 10:29:35 -0500

    asdf

---
+++ messaging.js
@@ -0,0 +1,5 @@
+function getHeavyTampon() {
+    /*const heavytampon = document.querySelector("#input-heavy-tampons").value;
+    console.log(heavytampon);*/
+    localStorage.setItem('heavytampon', document.querySelector("#input-heavy-tampons").value);
+}
